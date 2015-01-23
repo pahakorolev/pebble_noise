4f8a1e19f43b7f6b5941ec3f8a99c55ec1e756c0
Ptaha <pahakorolev@gmail.com>
2015-01-23 13:47:36 +0300

    frame-buffer implementation
    ✔ Added drawing of noise using frame-buffer
    ✔ More faster drawing method is set as default (frame-buffer)
    ✔ Switching between drawing modes can be changed by buttons pressing

--- src/main.c
+++ src/main.c
@@ -3,14 +3,16 @@
 static Window *window;
 static Layer *canvas=NULL;
 static AppTimer *timer;
+static int current_update_proc = 0;
 
 static void update(){
 	if (canvas!=NULL)
 		layer_mark_dirty(canvas);
+	timer = app_timer_register(1000/30, update, NULL);
 }
 
-static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(this_layer);
+static void update_proc_draw_pixel(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
   GPoint pixel = GPoint(0, 0);
 	for (int i=0; i<bounds.size.w; i++){
 		for (int j=0; j<bounds.size.h; j++){
@@ -20,8 +22,24 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
 			graphics_draw_pixel(ctx, pixel);
 		}
 	}
-	timer = app_timer_register(20, update, NULL);
 }
+
+static void update_proc_frame_buffer(Layer *layer, GContext *ctx){
+	GBitmap *fb = graphics_capture_frame_buffer(ctx);
+	GRect bounds=fb->bounds;
+	uint8_t *pos = (uint8_t *)fb->addr;
+	for (int16_t y = bounds.origin.y; y < bounds.size.h; y++) {
+		for (int16_t x = bounds.origin.x; x < fb->row_size_bytes; x++){
+			memset(++pos, rand(), 1);
+		}
+  }
+	graphics_release_frame_buffer(ctx, fb);
+}
+
+static LayerUpdateProc update_procs[] = {
+    update_proc_frame_buffer,
+    update_proc_draw_pixel
+};
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
@@ -30,12 +48,24 @@ static void main_window_load(Window *window) {
   canvas = layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
   layer_add_child(window_layer, canvas);
   // Set the update_proc
-  layer_set_update_proc(canvas, canvas_update_proc);
+  layer_set_update_proc(canvas, update_procs[current_update_proc]);
+	update();
 }
 
 static void main_window_unload(Window *window) {
   // Destroy Layer
   layer_destroy(canvas);
+}
+
+static void toggle_update_proc_click_handler(ClickRecognizerRef recognizer, void *context) {
+  current_update_proc = (current_update_proc + 1) % ARRAY_LENGTH(update_procs);
+  layer_set_update_proc(canvas, update_procs[current_update_proc]);
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, toggle_update_proc_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, toggle_update_proc_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, toggle_update_proc_click_handler);
 }
 
 static void init(void) {
@@ -48,7 +78,7 @@ static void init(void) {
 	// Get rid of the top bar
 	window_set_fullscreen(window, true);
   window_stack_push(window, false);
-	//timer = app_timer_register(100, update, NULL);
+	window_set_click_config_provider(window, click_config_provider);
 }
 
 static void deinit(void) {
